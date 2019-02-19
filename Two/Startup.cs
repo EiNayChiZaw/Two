ad37c137181580777d9c769c4e4e394fb3cf2b98
EiNayChiZaw <hisokakuroyuri125@gmail.com>
2019-02-19 15:24:28 +0630

    first commit

---
+++ Two/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Two.Startup))]
+namespace Two
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
